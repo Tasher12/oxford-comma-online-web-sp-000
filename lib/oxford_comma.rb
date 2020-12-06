adb06de47696b4caf02a39dcdbf5eda6264f89c8
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-12-06 19:22:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
   array.each {|i| i}.join
   array.each {|i| i}.join(" and ")
-  array.each {|i| i}..to_sentence(words_connector: ' , ', last_word_connector: ' and ')]
+  array.each {|i| i}..to_sentence(words_connector: ' , ', last_word_connector: ' and ')
 end
