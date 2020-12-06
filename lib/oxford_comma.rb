6ac9136a012715d6fb103ef773eecfef737c28b1
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-12-06 19:32:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,7 @@
 def oxford_comma(array)
   array.each {|i| i}.join
   
-end
-
-def oxford_comma(array)
   array.each {|i| i}.join(" and ")
-end
 
-def oxford_comma(array)
-  array.each {|i| i[-1] << " and "}
+  array[-1] = " and"
 end 
